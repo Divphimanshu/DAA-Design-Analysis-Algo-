2acbf08de0f3093cc04d81603d41bdf19118edbc
Divyaprakash Himanshu <divphimanshu27@gmail.com>
2025-07-04 14:23:26 +0530

    Add files via upload

---
+++ FloydWarshell/floyd.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#define V 4
+#define INF 99999  // Representation of infinity
+
+// Function to print the solution matrix
+void printSolution(int dist[V][V]) {
+    printf("Shortest distances between every pair of vertices:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Floyd-Warshall algorithm
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V];
+
+    // Initialize the solution matrix same as input graph matrix
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    // Add all vertices one by one to the set of intermediate vertices
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                // If vertex k is on the shortest path from i to j,
+                // then update the value of dist[i][j]
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    printSolution(dist);
+}
+
+// Driver code
+int main() {
+    int graph[V][V] = {
+        {0,   5,  INF, 10},
+        {INF, 0,   3, INF},
+        {INF, INF, 0,   1},
+        {INF, INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+    return 0;
+}
